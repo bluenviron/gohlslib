1042ce672f78396c298d0cd349d3d89d33f34226
aler9 <46489434+aler9@users.noreply.github.com>
2023-07-30 17:19:39 +0200

    use mediacommon/pkg/mpegts in examples

---
+++ examples/muxer/packet_conn_reader.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net"
+)
+
+type packetConnReader struct {
+	net.PacketConn
+}
+
+func newPacketConnReader(pc net.PacketConn) *packetConnReader {
+	return &packetConnReader{
+		PacketConn: pc,
+	}
+}
+
+func (r *packetConnReader) Read(p []byte) (int, error) {
+	n, _, err := r.PacketConn.ReadFrom(p)
+	return n, err
+}
