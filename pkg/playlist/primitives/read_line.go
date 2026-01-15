b0c62226e36bf57df797cd62b535554f0a967628
aler9 <46489434+aler9@users.noreply.github.com>
2026-01-15 23:23:44 +0100

    additional changes

--- pkg/playlist/primitives/read_line.go
+++ pkg/playlist/primitives/read_line.go
@@ -6,12 +6,10 @@ import (
 
 // ReadLine reads a line from a string. It returns the line and the remaining string.
 func ReadLine(s string) (string, string) {
-	i := strings.IndexByte(s, '\n')
-	if i < 0 {
+	line, remaining, found := strings.Cut(s, "\n")
+	if !found {
 		return s, ""
 	}
-
-	line, remaining := s[:i], s[i+1:]
 
 	if len(line) != 0 && line[len(line)-1] == '\r' {
 		line = line[:len(line)-1]
